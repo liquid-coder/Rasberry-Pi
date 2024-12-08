5ecb6cd063e40a6362c3b38728c999d258d2bfc6
Md. Fardeen Islam <mdfardeenislam28@gmail.com>
2024-12-08 14:17:52 +0600

    Update code.py

--- Basic Servo Connection/code.py
+++ Basic Servo Connection/code.py
@@ -4,7 +4,7 @@ from time import sleep
 Device.pin_factory = PiGPIOFactory()
 s = AngularServo(17,min_angle = 0, max_angle = 180,min_pulse_width=0.5/1000,max_pulse_width = 25/10000)
 while True:
-s.angle=120
-sleep(1)
-s.angle=60 
-sleep(1)
+  s.angle=120
+  sleep(1)
+  s.angle=60 
+  sleep(1)
